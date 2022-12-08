b4c3ef815336d9fe729b2cab4ec6bf9b21d7619c
Onkar Bartakke <54997502+onkarbartakke@users.noreply.github.com>
2022-12-08 13:14:31 +0530

    Add files via upload

--- Back-Tracking and Recursion/wine_selling_RECURSION.cpp
+++ Back-Tracking and Recursion/wine_selling_RECURSION.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
-int profit(int a[],int n,int y,int left,int right)
+int profit(int a[], int n, int y, int left, int right)
 {
-    if(left==right)
-    {
-        return y*a[left];
-    }
-    else
-    {
-        return max(a[left]*y+profit(a,n,y+1,left+1,right),a[right]*y+profit(a,n,y+1,left,right-1));
-    }
-}
-int main() {
-	int n,i;
-	cin>>n;
-	int a[n];
-	for(i=0;i<n;i++)
+	if (left == right)
 	{
-	    cin>>a[i];
+		return y * a[left];
 	}
-	
-	int ans=profit(a,n,1,0,n-1);
-	cout<<ans;
+	else
+	{
+		return max(a[left] * y + profit(a, n, y + 1, left + 1, right), a[right] * y + profit(a, n, y + 1, left, right - 1));
+	}
+}
+int main()
+{
+	int n, i;
+	cin >> n;
+	int a[n];
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	int ans = profit(a, n, 1, 0, n - 1);
+	cout << ans;
 	return 0;
 }
